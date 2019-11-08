dbc94fb6f40aa99554d162050121c71969dad666
Luke Allen <allenln@dulwich.org.uk>
2019-11-08 14:33:41 +0000

    done-ish the continuous game
    done main functuality

--- sprites.py
+++ sprites.py
@@ -14,17 +14,17 @@ class Block (pygame.sprite.Sprite):
         self.OG_image = pygame.Surface([width,height])
         self.center = self.OG_image.get_rect().center
         self.rect = self.OG_image.get_rect(center = self.center)
-        self.image_angle = random.randrange(0,360)
+        self.image_angle = 0#random.randrange(0,360)
         self.image = pygame.transform.rotate(self.OG_image, self.image_angle)
     def spin(self):
-        self.image_angle += 2
+        self.image_angle += 10
         self.image = pygame.transform.rotate(self.OG_image, self.image_angle)
         self.rect = self.image.get_rect(center = self.center)
     def move(self):
-        angle = self.image_angle
-        self.rect.x += math.sin(self.image_angle)*speed*-1
-        self.rect.y += math.cos(self.image_angle)*speed*-1
-        self.center = (self.center[0] + math.sin(self.image_angle)*speed*-1, self.center[1] + math.cos(self.image_angle)*speed*-1)
+        rad = math.radians(self.image_angle)
+        self.rect.x += math.sin(rad)*speed*-1
+        self.rect.y += math.cos(rad)*speed*-1
+        self.center = (self.center[0] + math.sin(rad)*speed*-1, self.center[1] + math.cos(rad)*speed*-1)
 
 pygame.init()
 
@@ -33,17 +33,21 @@ screen_width = 700
 screen_height = 500
 screen = pygame.display.set_mode([screen_width,screen_height])
 
-block_list = pygame.sprite.Group()
+blocks = pygame.sprite.Group()
+block_list = []
+cblock_hit_list = []
 all_sprites_list = pygame.sprite.Group()
 
 for i in range(4):
     block = Block(20,15)
     block.rect.x = random.randrange(0,650)
-    block.rect.y = i*100
-    block.OG_image = pygame.transform.scale(pygame.image.load("wood.jpg"), (100, 100))
-    block_list.add(block)
+    block.rect.y = (i*200) - 400
+    block.OG_image = pygame.transform.scale(pygame.image.load("wood.jpg"), (20, 15))
+    blocks.add(block)
+    block_list.append(block)
     all_sprites_list.add(block)
 
+target = 3
 player = Block(20, 20)
 player.rect.x = 325
 player.rect.y = 300
@@ -55,6 +59,7 @@ all_sprites_list.draw(screen)
 spin = False
 move = False
 done = False
+start = True
 clock = pygame.time.Clock()
 score = 0
 
@@ -62,7 +67,7 @@ while done == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and start == True:
         player.spin()
         spin = True
     elif (spin == True and event.type != pygame.KEYDOWN) or (move == True):
@@ -70,16 +75,24 @@ while done == False:
         move = True
         player.move()
 
-    
     screen.fill(white)
     
-    block_hit_list = pygame.sprite.spritecollide(player,block_list,True)
-    
-    for block in block_hit_list:
-        score += 1
-        print(score)
-        if score == 4:
+    blocks_hit = pygame.sprite.spritecollide(player,blocks,False)
+        
+    if len(blocks_hit) > 0:
+        if blocks_hit[-1] == block_list[target]:
+            block_list[target].rect.x = random.randrange(0, 650)
+            block_list[target].rect.y -= 800
+            target -= 1
+            if target == -1:
+                target = 3
+            score += 1
+            print(score)
+        else:
             done = True
+
+    for sprite in all_sprites_list:
+        sprite.rect.y += 2
     
     all_sprites_list.draw(screen)
     
