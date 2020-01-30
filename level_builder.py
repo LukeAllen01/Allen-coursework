85a5387edbde2608b053eb07e57f11a7b98b7390
Luke Allen <allenln@dulwich.org.uk>
2020-01-30 19:36:07 +0000

    mistake
    when trying to make drag function, constantly creates a new image when moving it

--- level_builder.py
+++ level_builder.py
@@ -9,6 +9,7 @@ clock = pygame.time.Clock()
 WHITE = (255,255,255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+BLACK = (0,0,0)
 done = False
 klc = RED
 blc = BLUE
@@ -23,9 +24,7 @@ class Block (pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.width = width
         self.height = height
-        print(image)
-        self.h = pygame.image.load(image)
-        self.OG_image = pygame.transform.scale(self.h, (self.width,self.height))
+        self.OG_image = pygame.transform.scale(pygame.image.load(image), (self.width,self.height))
         self.center = self.OG_image.get_rect().center
         self.rect = self.OG_image.get_rect(center = self.center)
         self.image_angle = random.randrange(0,360)
@@ -54,8 +53,57 @@ class Create(Block):
     def __init__(self, image):
         super().__init__(image)
         self.image_angle = 0
-        self.rect.x = 0
-        self.rect.y = 0
+        self.image = pygame.transform.rotate(self.OG_image, self.image_angle)
+        self.rect.x = 10
+        self.rect.y = 10
+        self.select = False
+        self.dragstart = False
+        self.og_x = 0
+        self.og_y = 0
+        self.drag_og_x = 0
+        self.drag_og_y = 0
+    def selecter(self):
+        if (event.type == pygame.MOUSEBUTTONDOWN):
+            print(1)
+            for i in all_sprites_list:
+                i.select = False
+            if (pygame.mouse.get_pos()[0] > self.rect.x) and (pygame.mouse.get_pos()[0] <(self.rect.x + self.width)) and ((pygame.mouse.get_pos()[1] > self.rect.y) and (pygame.mouse.get_pos()[1] < (self.rect.y + self.height))):
+                self.select = True
+        if self.select == True:
+            pygame.draw.rect(screen, BLACK, [self.rect.x - 5, self.rect.y - 5, self.width + 10, self.height + 10], 2)
+            #corners
+            pygame.draw.circle(screen, WHITE, (self.rect.x - 5, self.rect.y - 5), 5)                                        #top-left
+            pygame.draw.circle(screen, WHITE, (self.rect.x - 5, self.rect.y + self.height + 5), 5)                          #bottom-left
+            pygame.draw.circle(screen, WHITE, (self.rect.x + self.width + 5, self.rect.y - 5), 5)                           #top-right
+            pygame.draw.circle(screen, WHITE, (self.rect.x + self.width + 5, self.rect.y + self.height + 5), 5)             #bottom-right
+
+            pygame.draw.circle(screen, BLACK, (self.rect.x - 5, self.rect.y - 5), 5, 1)                             
+            pygame.draw.circle(screen, BLACK, (self.rect.x - 5, self.rect.y + self.height + 5), 5, 1)
+            pygame.draw.circle(screen, BLACK, (self.rect.x + self.width + 5, self.rect.y - 5), 5, 1)
+            pygame.draw.circle(screen, BLACK, (self.rect.x + self.width + 5, self.rect.y + self.height + 5), 5, 1)
+            #sides
+            pygame.draw.circle(screen, WHITE, (int(self.rect.x - 3 + 0.5*self.width), self.rect.y - 5), 5)
+            pygame.draw.circle(screen, WHITE, (self.rect.x - 5, int(self.rect.y + 0.5*self.height - 3)), 5)
+            pygame.draw.circle(screen, WHITE, (int(self.rect.x + 0.5*self.width + 3), self.rect.y + self.height + 5), 5)
+            pygame.draw.circle(screen, WHITE, (self.rect.x + self.width + 5, int(self.rect.y + 0.5*self.height + 3)), 5)
+
+            pygame.draw.circle(screen, BLACK, (int(self.rect.x - 3 + 0.5*self.width), self.rect.y - 5), 5, 1)
+            pygame.draw.circle(screen, BLACK, (self.rect.x - 5, int(self.rect.y + 0.5*self.height - 3)), 5, 1)
+            pygame.draw.circle(screen, BLACK, (int(self.rect.x + 0.5*self.width + 3), self.rect.y + self.height + 5), 5, 1)
+            pygame.draw.circle(screen, BLACK, (self.rect.x + self.width + 5, int(self.rect.y + 0.5*self.height + 3)), 5, 1)
+    def drag(self):
+        if event.type == pygame.MOUSEBUTTONDOWN and self.select == True and self.dragstart == False and (pygame.mouse.get_pos()[0] > self.rect.x) and (pygame.mouse.get_pos()[0] <(self.rect.x + self.width)) and ((pygame.mouse.get_pos()[1] > self.rect.y) and (pygame.mouse.get_pos()[1] < (self.rect.y + self.height))):
+            self.dragstart = True
+            self.drag_og_x = pygame.mouse.get_pos()[0]
+            self.drag_og_y = pygame.mouse.get_pos()[1]
+            self.og_x = self.rect.x
+            self.og_y = self.rect.y
+        if self.dragstart == True:
+            self.rect.x = self.og_x + (pygame.mouse.get_pos()[0] - self.drag_og_x)
+            self.rect.y = self.og_y + (pygame.mouse.get_pos()[1] - self.drag_og_y)
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.dragstart == False
+            
         
 while not done:
     for event in pygame.event.get():
@@ -73,7 +121,9 @@ while not done:
         elif event.type == pygame.KEYDOWN and event.key == FinalBlockKey:
             final = Create("wood.jpg")
             all_sprites_list.add(final)
-
+    for i in all_sprites_list:
+        i.selecter()
+        i.drag()
     all_sprites_list.draw(screen)
     clock.tick(20)
 
